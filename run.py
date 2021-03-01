028b0a9dc325191a9b3ad4fc907e615aeacd4bee
Qifan Deng <plocircle@live.com>
2021-03-01 11:33:21 +0800

    Change logging to english. TODO: fix sync issue

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
+import sys
 from os import listdir
 from os.path import isfile, join
 from imghdr import what
 import multiprocessing as mp
+from PIL import Image
 from functools import partial
 from tqdm import tqdm
-from shutil import move, copy
-from imagededup.methods import DHash
+from shutil import copy
+from imagehash import dhash
 
 
 class Filter:
@@ -33,19 +35,18 @@ class Filter:
         if not os.path.exists(out_sub_path):
             os.mkdir(out_sub_path)
         file_list = [os.path.join(target_path, f) for f in listdir(target_path) if isfile(join(target_path, f))]
-        print("[*] 共有 {} 个文件，使用 {} 线程处理。".format(len(file_list), self.n_proc))
+        print("[*] There are {} image. Going to run with {} threads.".format(len(file_list), self.n_proc))
         return target_sub_path, out_sub_path, file_list
 
     def hash_filter_handler(self, hash_set, lock, out_sub_path, files_chunk):
-        for file_path in tqdm(files_chunk, desc="正在检验是否拥有唯一哈希", unit=" 文件"):
+        for file_path in tqdm(files_chunk, desc="Filtering using hash", unit="file"):
             f = open(file_path, 'rb')
             content = f.read()
-            f_hash = hash(content)
+            hash_res = hash(content)
             f.close()
-
             lock.acquire()
-            if f_hash not in hash_set:
-                hash_set.add(f_hash)
+            if hash_res not in hash_set:
+                hash_set.add(hash_res)
                 lock.release()
                 file_name = os.path.basename(file_path)
                 self.copy(file_path, os.path.join(out_sub_path, file_name))
@@ -53,18 +54,16 @@ class Filter:
                 lock.release()
 
     def hash_filter(self, target_sub_path, out_sub_path):
-
         target_sub_path, out_sub_path, file_list = self.mkdir(target_sub_path, out_sub_path)
-
         unique_hash_set = set()
         lock = mp.Manager().Lock()
         with mp.Pool(processes=self.n_proc) as pool:
             pool.map(partial(self.hash_filter_handler, unique_hash_set, lock, out_sub_path), self.chunks(file_list))
-        print("[*] 唯一哈希文件皆已存于：{}".format(out_sub_path))
+        print("[*] Files with unique hash have been copied to：{}".format(out_sub_path))
 
     def valid_handler(self, out_sub_path, files_chunk):
         valid_types = {"jpeg", "jpg", "png"}
-        for file_path in tqdm(files_chunk, desc="正在检验是否为图像文件", unit=" 文件"):
+        for file_path in tqdm(files_chunk, desc="Filtering using file type", unit="file"):
             file_type = what(file_path)
             if file_type in valid_types:
                 base_name = os.path.basename(file_path)
@@ -78,10 +77,13 @@ class Filter:
 
         with mp.Pool(processes=self.n_proc) as pool:
             pool.map(partial(self.valid_handler, out_sub_path), self.chunks(file_list))
-        print("[*] 有效图像文件皆已存于：{}".format(out_sub_path))
+        print("[*] Images have been copied to: {}".format(out_sub_path))
 
     def size_handler(self, out_sub_path, threshold, files_chunk):
-        for file_path in tqdm(files_chunk, desc="正在检验是否大于 {}KB".format(threshold / 1024), unit=" 文件"):
+        for file_path in tqdm(
+                files_chunk,
+                desc="Filtering files with size less than {}KB".format(threshold / 1024),
+                unit="files"):
             if os.path.getsize(file_path) > threshold:
                 file_name = os.path.basename(file_path)
                 self.copy(file_path, os.path.join(out_sub_path, file_name))
@@ -92,61 +94,71 @@ class Filter:
 
         with mp.Pool(processes=self.n_proc) as pool:
             pool.map(partial(self.size_handler, out_sub_path, threshold), self.chunks(file_list))
-        print("[*] 大于 {}KB 图像文件皆已存于：{}".format(threshold / 1024, out_sub_path))
+        print("[*] Images greater than {}KB have been copied to: {}".format(threshold / 1024, out_sub_path))
 
     def diff_hash_filter_handler(self, hash_set, lock, out_sub_path, files_chunk):
-        d_hash_method = DHash()
-        for file_path in tqdm(files_chunk, desc="正在检验是否拥有差异哈希", unit=" 图像"):
-            d_hash = d_hash_method.encode_image(image_file=file_path)
-
+        for file_path in tqdm(
+                files_chunk,
+                desc="Filtering using dhash",
+                unit="image"):
+            image = Image.open(file_path)
+            d_hash = dhash(image)
+            hash_res = d_hash.__hash__()
             lock.acquire()
-            if d_hash not in hash_set:
-                hash_set.add(d_hash)
+            print(hash_set)
+            if hash_res not in hash_set:
+                hash_set.add(hash_res)
                 lock.release()
                 file_name = os.path.basename(file_path)
                 self.copy(file_path, os.path.join(out_sub_path, file_name))
             else:
                 lock.release()
 
+    def _filter_handler(self, lock, hash_res, hash_set, file_path, out_sub_path):
+        lock.acquire()
+        print(hash_set)
+        if hash_res not in hash_set:
+            hash_set.add(hash_res)
+            lock.release()
+            file_name = os.path.basename(file_path)
+            self.copy(file_path, os.path.join(out_sub_path, file_name))
+        else:
+            lock.release()
+
     def diff_hash_filter(self, target_sub_path, out_sub_path):
-
         target_sub_path, out_sub_path, file_list = self.mkdir(target_sub_path, out_sub_path)
-
         unique_hash_set = set()
         lock = mp.Manager().Lock()
-        with mp.Pool(processes=self.n_proc) as pool:
+        with mp.Pool(processes=1) as pool:
             pool.map(partial(self.diff_hash_filter_handler, unique_hash_set, lock, out_sub_path),
                      self.chunks(file_list))
-        print("[*] 唯一差异哈希图像皆已存于：{}".format(out_sub_path))
+        print("[*] Images with unique dhash have been copied to: {}".format(out_sub_path))
 
     @staticmethod
     def hamming_d(bin_a, bin_b):
         """Calculate the Hamming distance between two bit strings"""
-        assert len(bin_a) == len(bin_b)
-        count, z = 0, int(bin_a, 2) ^ int(bin_b, 2)
+        count, z = 0, bin_a ^ bin_b
         while z:
             count += 1
             z &= z - 1  # magic!X
         return count
 
     def hamming_filter(self, target_sub_path, out_sub_path, threshold=0.1):
-
         target_sub_path, out_sub_path, file_list = self.mkdir(target_sub_path, out_sub_path)
-        d_hash_method = DHash()
         keep_list = set()
-        keep_list.add(d_hash_method.encode_image(image_file=file_list[0]))
+        image = Image.open(file_list[0])
+        keep_list.add(dhash(image).__hash__())
         self.copy(file_list[0], os.path.join(out_sub_path, os.path.basename(file_list[0])))
 
-        for file_path in tqdm(file_list, desc="差异哈希汉明距离", unit="图像"):
-            d_hash = d_hash_method.encode_image(image_file=file_path)
+        for file_path in tqdm(file_list, desc="Filtering using Hamming Distance", unit="image"):
+            image = Image.open(file_path)
+            d_hash = dhash(image).__hash__()
             if d_hash is None:
                 continue
-            d_hash_bin = bin(int(d_hash, 16))[2:].zfill(64)
             should_save = True
 
             for now_have in keep_list:
-                now_have = bin(int(now_have, 16))[2:].zfill(64)
-                diff = self.hamming_d(now_have, d_hash_bin)
+                diff = self.hamming_d(now_have, d_hash)
                 diff_ratio = diff / 64
                 if diff_ratio < threshold:
                     should_save = False
@@ -155,13 +167,17 @@ class Filter:
                 file_name = os.path.basename(file_path)
                 keep_list.add(d_hash)
                 self.copy(file_path, os.path.join(out_sub_path, file_name))
-        print("[*] 经汉明过滤图像皆已存于：{}".format(out_sub_path))
+        print("[*] Images filtered by Hamming Distance have been copied to: {}".format(out_sub_path))
 
 
 if __name__ == '__main__':
-    new_filter = Filter(work_place="../../dataset/images/filtered", n_proc=4, keep_original=True)
-    # new_filter.size_filter("unique_dhash", "big_enough", threshold=10 * 1024)
-    # new_filter.valid_filter("big_enough", "valid_picture")
-    # new_filter.hash_filter("valid_picture", "unique_hash")
-    # new_filter.diff_hash_filter("unique_hash", "unique_dhash")
+    new_filter = Filter(
+        work_place=sys.argv[1],
+        n_proc=4,
+        keep_original=True)
+    new_filter.size_filter("./", "big_enough", threshold=10 * 1024)
+    new_filter.valid_filter("big_enough", "valid_picture")
+    new_filter.hash_filter("valid_picture", "unique_hash")
+    new_filter.diff_hash_filter("unique_hash", "unique_dhash")
     new_filter.hamming_filter("unique_dhash", "hamming", 0.1)
+    # new_filter.valid_filter("big_enough", "valid_picture")
